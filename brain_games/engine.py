8da2f5629e8192bbc4d8fd33104d9dfe3819b16d
HidTired <Dentanyabonya@mail.ru>
2025-12-11 17:51:33 +0300

    fixed bugs

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -1,7 +1,7 @@
 from brain_games.utils import welcome_user, prompt_string
 
 def play(question_generator):
-    player_name = welcome_user()  # Приветствуем пользователя
+    player_name = welcome_user()
     rounds_to_win = 3
     wins = 0
 
